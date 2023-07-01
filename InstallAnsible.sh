44a685cbc0dbe4b27e178484a769358906f2b081
SAMMY <119563245+acheampong-tech@users.noreply.github.com>
2023-07-01 09:45:33 -0500

    Create InstallAnsible.sh
    https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html

---
+++ InstallAnsible.sh
@@ -0,0 +1,14 @@
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+python3 get-pip.py --user
+
+python3 -m pip install --user ansible
+
+ansible --version
+
+python3 -m pip show ansible
+
+
+
+
+
